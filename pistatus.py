8363ad4b9baccc05342f281d88e374e942b43e42
Donovan Powers <dontpowers@hotmail.com>
2018-01-21 18:52:12 -0600

    AWS initizliation

--- pistatus.py
+++ pistatus.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from time import sleep
+import boto3
 
 
 
@@ -7,7 +8,11 @@ from time import sleep
 def init():
 	print("initializing PiCam.")
 	global camera
+	global s3
+	#initialize camera object
 	camera = PiCamera()
+	#initialize and connect to AWS
+	s3 = boto3.resource('s3')
 	takePicture('test')
 
 
@@ -15,10 +20,13 @@ def takePicture(name):
 	camera.start_preview()
 	sleep(5)
 	camera.capture('pics/' + name + '.png')
+	#upload to AWS instead of writing to file
+	s3.Bucket('my-bucket').put_object(Key=(name + '.png'), Body=data)
 	camera.stop_preview()
 	print ('took picture')
 
 
+def uploadPhotoAWS(buffer)
 
 
 
