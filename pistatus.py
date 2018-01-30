d7d36cc832c21d7c2f2f48e35e68ee7aa827ff74
Donovan Powers <dontpowers@hotmail.com>
2018-01-29 20:57:32 -0600

    condensed all api calls to be hosted on pi instead

--- pistatus.py
+++ pistatus.py
@@ -3,6 +3,8 @@ from time import sleep
 import boto3
 import datetime
 import io
+import json
+import pprint
 
 
 
@@ -10,18 +12,20 @@ import io
 def init():
 	print("initializing PiCam.")
 	global camera
-	global s3
 	global mostRecentStatusID
-	mostRecentStatusID = 1
+	global DATABASE
+    global pp
+    pp = pprint.PrettyPrinter(indent=4)
+    DATABASE = loadDB()
+    mostRecentStatusID = len(DATABASE)
+    print('Previous Status #: ' + str(mostRecentStatusID))
 	#initialize camera object
 	camera = PiCamera()
-	#initialize and connect to AWS
-	s3 = boto3.resource('s3')
+	print("Server initialized...")
 	takePicture()
-	#get most recent server info
-	update()
 
 
+#takes pictures, returns aws key for file
 def takePicture():
 	#initialize buffer
 	data = io.BytesIO()
@@ -33,10 +37,11 @@ def takePicture():
 	print ('took picture')
 	#upload to AWS
 	data.seek(0)
+	s3 = boto3.resource('s3')
 	s3.Bucket('pistatus').put_object(Key=(name), Body=data)
 	print ('uploaded picture: ' + name)
-	#update webserver
-	#submitStatus()
+	return name
+
 
 
 
@@ -46,16 +51,60 @@ def generateFilename():
 
 
 
-#query server to get most recent values
-#mostRecentStatus
-def update():
-	return 1
+##################SERVER LOGIC
 
-def submitStatus():
-	return 1
+def serverInit():
+    
+
+def shutdownServer():
+    saveDB()
+
+
+def recStatus(status):
+    prevStatus += 1
+    DATABASE = status
+
+
+def getLastStatus():
+    status = DATABASE[str(prevStatus)]
+    return status
+    
+
+def getStatus(id):
+    return DATABASE[id]
+
+##########################DB OPERATIONS#################################
+
+
+#read database from disk into memory
+def loadDB():
+    print('Loading Database...')
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket='pistatus', Key='data.json')
+    database = json.loads(response['Body'].read().decode("UTF-8"))
+    pp.pprint(database)
+    DATABASE = database
+    print('Finished Loading ')
+    return database
+
+
+#write db from memory to disk
+def saveDB():
+    buf = json.dumps(DATABASE).encode('UTF-8')
+    s3 = boto3.resource('s3')
+    s3.Bucket('pistatus').put_object(Key='data.json', Body=buf)
+    print('Wrote DB to disk...')
+
+
+
+############################################## tests
+
 
 
 
 
 
 init()
+loadDB()
+sleep(5)
+shutdownServer()
